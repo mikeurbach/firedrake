31a55ed22283fba0216d6c72b80dafa6e1aa0d3c
Andrew K. Jankowski <andrewkjankowski@gmail.com>
2013-04-16 16:59:28 -0400

    cleaning up the git repository

--- chat/Debug/chat_client.d
+++
@@ -1,6 +0,0 @@
-chat_client.d chat_client.o: ../chat_client.c ../chat_client.h \
-  ../macros.h
-
-../chat_client.h:
-
-../macros.h:
--- chat/Debug/chat_server.d
+++
@@ -1,6 +0,0 @@
-chat_server.d chat_server.o: ../chat_server.c ../chat_server.h \
-  ../macros.h
-
-../chat_server.h:
-
-../macros.h:

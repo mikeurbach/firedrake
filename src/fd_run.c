81008446c65593f2fae578c89f09de9a451c73b1
Mike Urbach <mikeurbach@gmail.com>
2013-05-28 13:41:30 -0400

    Version 1.0.0

--- src/fd_run.c
+++ src/fd_run.c
@@ -1,3 +1,24 @@
+/* 
+    firedrake: an efficient library for writing websocket servers
+
+    Copyright (C) 2013 Mike Urbach, Matt Diephui, Drew Jankowski
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+		Report bugs to <mikeurbach@gmail.com>
+*/
+
 #define EV_STANDALONE 1
 #include "ev.c"
 #include "fd.h"
